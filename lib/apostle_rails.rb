6d4b85008e66e8f1814e34da2b84a01070537cda
Mal Curtis <mal@sitepoint.com>
2013-10-15 14:51:23 +1300

    Set apostle config if in rails test mode

--- lib/apostle_rails.rb
+++ lib/apostle_rails.rb
@@ -1,2 +1,9 @@
+require 'apostle'
 require "apostle_rails/version"
 require "apostle_rails/mailer"
+
+if defined?(Rails) && Rails.env.test?
+  Apostle.configure do |config|
+    config.deliver = false
+  end
+end
