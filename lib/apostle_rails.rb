62fb3750ce624d155b5e6cfa56f1eb3426aced3b
Sobin <sobisunny@gmail.com>
2017-03-26 16:39:56 +0530

    Added generator to create initializer

--- lib/apostle_rails.rb
+++ lib/apostle_rails.rb
@@ -2,8 +2,9 @@ require 'apostle'
 require "apostle_rails/version"
 require "apostle_rails/mailer"
 
+
 if defined?(Rails) && Rails.env.test?
   Apostle.configure do |config|
-    config.deliver = false
+    config.deliver = fals e
   end
 end
