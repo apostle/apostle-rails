ddc9f5e6c33099c539ac56cd1924c3ccadc6441e
Sobin <sobisunny@gmail.com>
2017-03-28 10:28:20 +0530

    Added generators

--- lib/apostle_rails.rb
+++ lib/apostle_rails.rb
@@ -1,10 +1,10 @@
 require 'apostle'
-require "apostle_rails/version"
-require "apostle_rails/mailer"
-
+require 'apostle_rails/version'
+require 'apostle_rails/mailer'
+require 'generators/apostle_rails/install_generator'
 
 if defined?(Rails) && Rails.env.test?
   Apostle.configure do |config|
-    config.deliver = fals e
+    config.deliver = false
   end
 end
--- lib/generators/apostle_rails/install_generator.rb
+++ lib/generators/apostle_rails/install_generator.rb
@@ -1,13 +1,13 @@
 require 'rails/generators'
+
 module ApostleRails
- # to create intializers
- module Generators
+  module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
-      namespace "apostle:install"
+      source_root File.expand_path('../templates', __FILE__)
+      namespace 'apostle:install'
       def copy_initializer_file
-        copy_file "initializer.rb", "config/initializers/apostle.rb"
+        copy_file 'initializer.rb', 'config/initializers/apostle.rb'
       end
-    end
- end
+     end
+  end
 end
